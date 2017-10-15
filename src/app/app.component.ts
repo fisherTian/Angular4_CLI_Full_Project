fd1f97ef901532d948c79f5f249dad88eaa8b8e7
xiaoyu <515301230@qq.com>
2017-10-15 21:20:36 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+
+@Component({
+  // tslint:disable-next-line
+  selector: 'body',
+  template: '<router-outlet></router-outlet>'
+})
+export class AppComponent {
+
+  constructor(private  router:Router){}
+
+  changeURL() {
+    var text = window.location.href;
+    text.toString();
+    var url = text.replace(/#/, "index.html#");
+    window.history.pushState({}, "0", url);
+  }
+
+  ngOnInit() {
+    this.router.events
+      .subscribe((event) => {
+        if (event instanceof NavigationEnd) { // 当导航成功结束时执行
+          console.log('NavigationEnd:', event);
+          this.changeURL();
+        }
+      });
+  }
+}
